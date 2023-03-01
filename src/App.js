605397d05ebd0df91a0aedae427d9706d5af466e
Greg Schwartz <greg@gregschwartz.net>
2023-03-01 15:06:43 -0700

    Routing is working!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contract from "./pages/Contract";
+import New from "./pages/New"
+
+//replace
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stupid Simple UI</h1>
       </header>
+      <div className='main'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<New />} />
+            <Route path='contracts/:chain/:address' element={<Contract />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
