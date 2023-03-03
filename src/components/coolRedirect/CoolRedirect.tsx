6343fae8143527c0bb38a3457dadf37eff624090
LuisGarcia <luis.garcia@tempostorm.com>
2023-03-02 21:53:25 -0600

    adding animation of checked tasks

--- src/components/coolRedirect/CoolRedirect.tsx
+++ src/components/coolRedirect/CoolRedirect.tsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./spinner.css";
 
 export default function CoolRedirect() {
+
+  const divRef = useRef<HTMLDivElement>(null);
+  let initialCheckState = [false, false, false, false];
+  const [checks, setChecks] = useState(initialCheckState);
+  const [counter, setCounter] = useState(0);
+
+
+
+  useEffect(() => {
+
+    console.log(divRef.current?.childNodes);
+    let newState = checks;   
+
+    const interval = setInterval(() => {
+      // do something         
+      newState[counter?counter:0] = true;
+      setChecks(newState);
+      setCounter(counter+1);
+    }, 2200)      
+
+    return () => {
+        clearInterval(interval);
+    }
+  
+ }, [checks, counter]);
+
   return (
+    <div className='container'>
     <div className='container'>
       <div className="spinner-container">
         <div className="loading-spinner">
         </div>
       </div>
+    </div>
+    <div className='container'>
+      <div className="checkListContainer" ref={divRef}>
+        <div className="checkListItem">
+          <div className={`checkboxItem  ${checks[0] ? "visible" : "unvisible"}`}>🗹</div>
+          <div className="textItem">Fetching contract address through a series of tubes</div>
+        </div>
+        <div className="checkListItem">
+          <div className={`checkboxItem ${checks[1] ? "visible" : "unvisible"}`}>🗹</div>
+          <div className="textItem">Making an offering to Vitalik </div>
+        </div>
+        <div className="checkListItem">
+          <div className={`checkboxItem ${checks[2] ? "visible" : "unvisible"}`}>🗹</div>
+          <div className="textItem">Analyzing ABI for methods and data</div>
+        </div>
+        <div className="checkListItem">
+          <div className={`checkboxItem ${checks[3] ? "visible" : "unvisible"}`}>🗹</div>
+          <div className="textItem">Generating your brand new UI</div>
+        </div>
+        {/* <div className="checkListItem">
+          <div className={`checkboxItem ${checks[1] ? "visible" : "unvisible"}`}></div>
+          <div className="textItem"></div>
+        </div> */}
+      </div>
+    </div>
     </div>
   );
 }
