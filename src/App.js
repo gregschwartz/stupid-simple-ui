f4de9541568cd04c8a6f1d9c8c75e8a3fed76197
Greg Schwartz <greg@gregschwartz.net>
2023-03-05 12:26:46 -0700

    Adding an examples page and link to that, instead of links in the header

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import Contract from "./pages/Contract";
 import New from "./pages/New";
 import ThemeEditor from './pages/theme/ThemeEditor';
+import Examples from './pages/Examples';
 
 //WalletConnect and Wagmi
 import { Web3Modal } from '@web3modal/react';
@@ -31,14 +32,7 @@ function App() {
           <a href="/">Stupid Simple UI</a>
         </h1>
         <div className='examples'>
-          Examples:{" "}
-          <a href="https://www.stupidsimpleui.com/contracts/Chain%2084531/0x8b2948d486336e9ecAe12D570469D0C62CdcbaC1">Base: Guestbook</a>
-          {" | "}
-          <a href="/contracts/Goerli/0xDB0d5dB07d42D5589230e40c3A51Ac8454D312d3">Goerli: Guestbook</a>
-          {" | "}
-          <a href="/contracts/goerli/0xcaFAa9C9662f2a7EaeceD891C039317035286540">Escrow</a>
-          {" | "}
-          <a href="/contracts/goerli/0xa4e4745a1066ac0faebe4e005793b172c69cc9c4">ERC20 Token</a>
+          <a href="/examples">Example UIs</a>
         </div>
         <div className='walletButton'>
           <Web3Button />
@@ -55,6 +49,7 @@ function App() {
               <Routes>
                 <Route path='/' element={<New />} />
                 <Route path='contracts/:chainName/:contractAddress' element={<Contract />} />
+                <Route path='examples' element={<Examples />} />
                 <Route path='themes' element={<ThemeEditor />} />
                 <Route path='themes/:themeId' element={<ThemeEditor />} />
                 <Route path='themes/new' element={<ThemeEditor />} />
---
+++ src/pages/Examples.js
@@ -0,0 +1,32 @@
+import './Examples.css'
+
+function Examples() {
+  
+  return (
+    <div className="exampleList prettyBackground">
+      <h2>Examples of UIs so you can see how this works</h2>
+      <div>
+        <h3>Ethereum Goerli Testnet</h3>
+        <ul>
+          <li><a href="/contracts/Goerli/0xDB0d5dB07d42D5589230e40c3A51Ac8454D312d3">Guestbook</a></li>
+          <li><a href="/contracts/goerli/0xcaFAa9C9662f2a7EaeceD891C039317035286540">Escrow</a></li>
+          <li><a href="/contracts/goerli/0xa4e4745a1066ac0faebe4e005793b172c69cc9c4">ERC20 Token</a></li>
+        </ul>
+      </div>
+      <div>
+        <h3>Coinbase Base Testnet</h3>
+        <ul>
+          <li><a href="https://www.stupidsimpleui.com/contracts/Chain%2084531/0x8b2948d486336e9ecAe12D570469D0C62CdcbaC1">Guestbook</a></li>
+        </ul>
+      </div>
+      <div>
+        <h3>Ethereum</h3>
+        <ul>
+          <li><a href="/contracts/Goerli/0xDB0d5dB07d42D5589230e40c3A51Ac8454D312d3">coming soon</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Examples;
