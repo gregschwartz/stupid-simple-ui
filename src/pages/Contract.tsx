e48e3de68683d01e7d91a36e01640ce86ee86e29
Greg Schwartz <greg@gregschwartz.net>
2023-03-07 15:39:32 -0800

    Fixed spinning icon when waiting for confirmation

--- src/pages/Contract.tsx
+++ src/pages/Contract.tsx
@@ -174,7 +174,7 @@ export default function Contract() {
         setExecutionStatus(ExecutionStatus.ERROR);
         html = `😭 Error, the contract didn't accept your request! Try different parameters or again later.`;
       } else if(result.hash) {
-        html = `<img src="/ethereum_icon48.png" height={24} width={24} className="loadingEthereum" /> Awaiting confirmation, up to 5 min wait (see wallet for info)`;
+        html = `<img src="/ethereum_icon48.png" height={24} width={24} class="loadingEthereum" /> Awaiting confirmation, up to 5 min wait (see wallet for info)`;
         console.log("awaiting confirm");
 
         result.wait().then(() => {
